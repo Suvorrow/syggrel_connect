0c7db97ffdd3acc8070dcdb5a80915c1785e09b7
Suvorrow <suvoraum@gmail.com>
2025-06-30 03:24:00 +0200

    Refactor and optimize the main.dart file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:syggrel_connect/config/localization/texts.dart';
 import 'package:syggrel_connect/config/screen_util_config.dart';
-import 'package:syggrel_connect/config/app_theme.dart';
-import 'package:syggrel_connect/pages/home_page.dart';
 import 'app/app_widget.dart';
 import '../app/routes.dart';
 
@@ -13,16 +9,9 @@ void main() {
 
   runApp(
     ProviderScope(
-      child: ScreenUtilInit(
-        designSize: screenUtilConfig.designSize,
-        minTextAdapt: screenUtilConfig.minTextAdapt,
-        splitScreenMode: screenUtilConfig.splitScreenMode,
-        builder: (context, child) => MaterialApp(
-          title: Texts.appTitle,
-          theme: AppTheme.appLightTheme,
-          darkTheme: AppTheme.appDarkTheme,
-          home: HomePage(),
-        ),
+      child: SYggRelConnectApp(
+        screenUtilConfig: screenUtilConfig,
+        router: router,
       ),
     ),
   );
